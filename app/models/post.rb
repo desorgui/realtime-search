fa060e77436d668bf8c21624e0e41c1c365f6c5f
Desor Guishny <desorguishny@gmail.com>
2023-01-17 09:13:23 -0500

    Update site styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
   include PgSearch::Model
+  belongs_to :user
   pg_search_scope :search_by_title_or_description, against: [:title, :description],
-                  using: { tsearch: { prefix: true } }
+                  using: {
+                    tsearch: { prefix: true, any_word: true },
+                    trigram: {
+                      only: [:title, :description],
+                      threshold: 0.25
+                    }
+                  }
 end
